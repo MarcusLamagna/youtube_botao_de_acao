40017de316df4712324333edb1ed2de35d7ffe07
MLamagna <marcus@mlamagna.com.br>
2023-11-23 01:01:55 -0300

    Ativando API

---
+++ lib/Api.dart
@@ -0,0 +1,3 @@
+const CHAVE_YOTUBE_API = "AIzaSyADPdy9f3AkLHiKFXmCsrqMNSODTkuf8F0";
+const ID_CANAL = "UCtzlsCBqmKL9LPGZzlYpPAw";
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/telas/Biblioteca.dart';
+import 'package:youtube/telas/EmAlta.dart';
+import 'package:youtube/telas/inicio.dart';
+import 'package:youtube/telas/inscricoes.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +12,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+    //Carregando páginas
+    List<Widget> telas = [Inicio(), EmAlta(), Inscricao(), Biblioteca()];
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.grey),
@@ -37,7 +45,34 @@ class _HomeState extends State<Home> {
               icon: Icon(Icons.account_circle)),
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _indiceAtual,
+          onTap: (indice) {
+            setState(() {
+              _indiceAtual = indice;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+                //backgroundColor: Colors.orange,
+                label: "Início",
+                icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+                //backgroundColor: Colors.red,
+                label: "Em alta",
+                icon: Icon(Icons.whatshot)),
+            BottomNavigationBarItem(
+                //backgroundColor: Colors.blue,
+                label: "Incrições",
+                icon: Icon(Icons.subscriptions)),
+            BottomNavigationBarItem(
+                //backgroundColor: Colors.green,
+                label: "Bibliotéca",
+                icon: Icon(Icons.folder)),
+          ]),
     );
   }
 }
